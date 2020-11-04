d5c29c0792410611529b5b3adf00966987003a4b
anzarshah <anzarshah43@gmail.com>
2020-11-04 12:18:45 +0530

    yoj

---
+++ precache-manifest.9c152b088fcb107e98c58967cad94bdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3d1c6a53e7d517cdcc9e4741b057ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d32b6c4dc23506cbb737",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "dd11781f1a0d69426bf6",
+    "url": "/static/js/2.cccd701c.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.cccd701c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d32b6c4dc23506cbb737",
+    "url": "/static/js/main.1a088657.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
